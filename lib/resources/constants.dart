2332b8eff94a240dcbcce6356fc94e5e2e2540b3
Viacheslav <viacheslav.sheviakov@gmail.com>
2020-09-12 02:15:20 +0300

    BASE-HOMEPAGE: Basic markup

---
+++ lib/resources/constants.dart
@@ -0,0 +1,4 @@
+class Constants {
+  static const exploreRusspassUrl = "https://explore.russpass.ru";
+  static const aviaTicketsUrl = "https://russpass.ru/avia-tickets";
+}
--- lib/ui/pages/home_page/home_page.dart
+++ lib/ui/pages/home_page/home_page.dart
@@ -1,4 +1,9 @@
+import 'package:ardico_test/resources/app_colors.dart';
+import 'package:ardico_test/resources/app_styles.dart';
+import 'package:ardico_test/resources/constants.dart';
+import 'package:ardico_test/resources/strings.dart';
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,12 +11,68 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  WebViewController _controller;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Home Page"),
+      body: SafeArea(
+        child: WebView(
+          initialUrl: Constants.exploreRusspassUrl,
+          onWebViewCreated: (WebViewController webViewController) {
+            _controller = webViewController;
+          },
+          javascriptMode: JavascriptMode.unrestricted,
+          initialMediaPlaybackPolicy: AutoMediaPlaybackPolicy.always_allow,
+        ),
       ),
+      bottomNavigationBar: _buildBottomBar(),
+    );
+  }
+
+  Widget _buildBottomBar() {
+    return Material(
+      color: AppColors.primary,
+      child: Row(
+        children: [
+          Expanded(
+            child: _buildExploreButton(),
+          ),
+          Expanded(
+            child: _buildAviaTicketsButton(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildExploreButton() {
+    return InkWell(
+      child: Container(
+        height: 50.0,
+        child: Center(
+          child: Text(
+            Strings.exploreButtonLabel.toUpperCase(),
+            style: AppStyles.bottomBarButton,
+          ),
+        ),
+      ),
+      onTap: () => _controller.loadUrl(Constants.exploreRusspassUrl),
+    );
+  }
+
+  Widget _buildAviaTicketsButton() {
+    return InkWell(
+      child: Container(
+        height: 50.0,
+        child: Center(
+          child: Text(
+            Strings.ticketsButtonLabel.toUpperCase(),
+            style: AppStyles.bottomBarButton,
+          ),
+        ),
+      ),
+      onTap: () => _controller.loadUrl(Constants.aviaTicketsUrl),
     );
   }
 }
